830ac514a89f0050d4f2e23745343ee04a9bdf27
Krithika V Christy <krithikavchristy@gmail.com>
2026-05-29 12:07:02 +0530

    Created host dashboard

--- host-app/src/app/app-routing.module.ts
+++ host-app/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: DashboardComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- host-app/src/app/app.module.ts
+++ host-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    MainLayoutComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
