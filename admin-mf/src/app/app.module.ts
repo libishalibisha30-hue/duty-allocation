3e775d2f4c88fcfb3adf28701d2922b84eaa910d
libishalibisha30-hue <libishalibisha30@gmail.com>
2026-05-29 12:03:58 +0530

    Completed admin dashboard and host layout

--- admin-mf/src/app/app.module.ts
+++ admin-mf/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { FacultyComponent } from './pages/faculty/faculty.component';
 import { DutyAllocationComponent } from './pages/duty-allocation/duty-allocation.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     FacultyComponent,
-    DutyAllocationComponent
+    DutyAllocationComponent,
+    SidebarComponent,
+    NavbarComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
